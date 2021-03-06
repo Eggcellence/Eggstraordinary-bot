f6f857a86b47659b0b7ab9218e8beec077a1347d
A7U <57374241+A7U@users.noreply.github.com>
2021-03-07 00:17:15 +0100

    syntax
    leo, why did you put - in line 46 lol

--- events/leveling.js
+++ events/leveling.js
@@ -43,7 +43,7 @@ module.exports = (message, egg) => {
                         });
                     }
                 });
-            }); -
+            });
             setTimeout(() => {
                 cooldown.delete(userid, guildid)
             }, 30000);
