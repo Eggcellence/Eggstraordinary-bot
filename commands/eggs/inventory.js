4a7f4b9b296bbf6269c5f99e9431f1f1b6542089
A7U <abdseyada04@gmail.com>
2021-04-17 22:51:32 +0200

    deprecating until fixed

--- commands/eggs/inventory.js
+++ commands/eggs/inventory.js
@@ -10,56 +10,56 @@ module.exports = {
 
         const guild = message.guild.id;
         const userid = message.author.id;
+        
+        // if (args[0]) {
+        //     message.guild.members.fetch({
+        //         query: args[0]
+        //     }).then(e => {
+        //         if(!e.first()) return message.channel.send(`I've searched everywhere but I couldn't find someone with the name of \`${args[0]}\``)
+        //         let user = e.first().user
 
-        if (args[0]) {
-            message.guild.members.fetch({
-                query: args[0]
-            }).then(e => {
-                if(!e.first()) return message.channel.send(`I've searched everywhere but I couldn't find someone with the name of \`${args[0]}\``)
-                let user = e.first().user
+        //         egg.query(`SELECT * FROM inventory WHERE guild = ${guild} AND userid = ${user.id}`, (err, rows) => {
+        //             if (err) throw err;
+        //             if (rows.length === 0) {
+        //                 message.reply(`I think ${user.username} is up to something. He has nothing in his inventory!`).then(m => m.delete({
+        //                     timeout: 5000
+        //                 }));
 
-                egg.query(`SELECT * FROM inventory WHERE guild = ${guild} AND userid = ${user.id}`, (err, rows) => {
-                    if (err) throw err;
-                    if (rows.length === 0) {
-                        message.reply(`I think ${user.username} is up to something. He has nothing in his inventory!`).then(m => m.delete({
-                            timeout: 5000
-                        }));
+        //             } else {
 
-                    } else {
+        //                 if (!rows[0].Chicken && !rows[0].Farm && !rows[0].Duck && !rows[0].Frog) return message.reply(`${user.username} have nothing in their inventory!`)
 
-                        if (!rows[0].Chicken && !rows[0].Farm && !rows[0].Duck && !rows[0].Frog) return message.reply(`${user.username} have nothing in their inventory!`)
-
-                        let embed = new Discord.MessageEmbed()
-                            .setTitle(`${user.username}'s Inventory`)
-                            .addFields(
-                                !rows[0].Chicken ? [] : {
-                                    name: '🐔',
-                                    value: `**${rows[0].Chicken}**`,
-                                    inline: true
-                                },
-                                !rows[0].Farm ? [] : {
-                                    name: '👩‍🌾',
-                                    value: rows[0].Farm,
-                                    inline: true
-                                },
-                                !rows[0].Duck ? [] : {
-                                    name: '🦆',
-                                    value: rows[0].Duck,
-                                    inline: true
-                                },
-                                !rows[0].Frog ? [] : {
-                                    name: '🐸',
-                                    value: rows[0].Frog,
-                                    inline: true
-                                }
-                            )
-                            .setColor('YELLOW')
-                            .setThumbnail(user.avatarURL())
-                        message.channel.send(embed)
-                    }
-                });
-            });
-        } else {
+        //                 let embed = new Discord.MessageEmbed()
+        //                     .setTitle(`${user.username}'s Inventory`)
+        //                     .addFields(
+        //                         !rows[0].Chicken ? [] : {
+        //                             name: '🐔',
+        //                             value: `**${rows[0].Chicken}**`,
+        //                             inline: true
+        //                         },
+        //                         !rows[0].Farm ? [] : {
+        //                             name: '👩‍🌾',
+        //                             value: rows[0].Farm,
+        //                             inline: true
+        //                         },
+        //                         !rows[0].Duck ? [] : {
+        //                             name: '🦆',
+        //                             value: rows[0].Duck,
+        //                             inline: true
+        //                         },
+        //                         !rows[0].Frog ? [] : {
+        //                             name: '🐸',
+        //                             value: rows[0].Frog,
+        //                             inline: true
+        //                         }
+        //                     )
+        //                     .setColor('YELLOW')
+        //                     .setThumbnail(user.avatarURL())
+        //                 message.channel.send(embed)
+        //             }
+        //         });
+        //     });
+        // } else {
             egg.query(`SELECT * FROM inventory WHERE guild = ${guild} AND userid = ${userid}`, (err, rows) => {
                 if (err) throw err;
                 if (rows.length === 0) {
@@ -104,5 +104,5 @@ module.exports = {
         }
 
     
-    }
+    // }
 };
--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -17,43 +17,43 @@ module.exports = {
         const discriminator = message.author.discriminator;
         const guildid = message.guild.id;
 
-        if (args[0]) {
-            message.guild.members.fetch({
-                query: args[0]
-            }).then(e => {
-                if (!e.first()) return message.channel.send(`I've searched everywhere but I couldn't find someone with the name of \`${args[0]}\``);
-                let user = e.first().user;
+        // if (args[0]) {
+        //     message.guild.members.fetch({
+        //         query: args[0]
+        //     }).then(e => {
+        //         if (!e.first()) return message.channel.send(`I've searched everywhere but I couldn't find someone with the name of \`${args[0]}\``);
+        //         let user = e.first().user;
 
-                egg.query(`SELECT * FROM leveling WHERE guild = ${guildid} AND userid = ${user.id}`, (err, rows) => {
+        //         egg.query(`SELECT * FROM leveling WHERE guild = ${guildid} AND userid = ${user.id}`, (err, rows) => {
 
-                    if (rows.length === 0) return message.reply(`<:woah:833072727224614963> Oh my god. This user never chatted here to gain XP!`).then(m => m.delete({
-                        timeout: 5000
-                    }));
+        //             if (rows.length === 0) return message.reply(`<:woah:833072727224614963> Oh my god. This user never chatted here to gain XP!`).then(m => m.delete({
+        //                 timeout: 5000
+        //             }));
 
-                    let xp = rows[0].xp
-                    let level = rows[0].level
-                    let reqxp = 5 * (level ** 2) + 50 * level + 100
-                    const rank = new canvacord.Rank()
-                        .setAvatar(user.avatarURL({format: 'png'}))
-                        .setCurrentXP(xp)
-                        .setRequiredXP(reqxp)
-                        .setLevel(level)
-                        .setRank(0, 0, display = false)
-                        .setLevelColor('#FFFFFF', '#f6b92b')
-                        .setStatus(user.presence.status)
-                        .setProgressBar("#f6b92b", "COLOR")
-                        .setUsername(user.username)
+        //             let xp = rows[0].xp
+        //             let level = rows[0].level
+        //             let reqxp = 5 * (level ** 2) + 50 * level + 100
+        //             const rank = new canvacord.Rank()
+        //                 .setAvatar(user.avatarURL({format: 'png'}))
+        //                 .setCurrentXP(xp)
+        //                 .setRequiredXP(reqxp)
+        //                 .setLevel(level)
+        //                 .setRank(0, 0, display = false)
+        //                 .setLevelColor('#FFFFFF', '#f6b92b')
+        //                 .setStatus(user.presence.status)
+        //                 .setProgressBar("#f6b92b", "COLOR")
+        //                 .setUsername(user.username)
 
-                        .setDiscriminator(user.discriminator);
+        //                 .setDiscriminator(user.discriminator);
 
-                    rank.build()
-                        .then(data => {
-                            const attachment = new Discord.MessageAttachment(data, "RankCard.png");
-                            message.channel.send(attachment);
-                        });
-                });
-            })
-        } else {
+        //             rank.build()
+        //                 .then(data => {
+        //                     const attachment = new Discord.MessageAttachment(data, "RankCard.png");
+        //                     message.channel.send(attachment);
+        //                 });
+        //         });
+        //     })
+        // } else {
             egg.query(`SELECT * FROM leveling WHERE guild = ${guildid} AND userid = '${userid}'`, (err, rows) => {
 
                 if (rows.length === 0) return message.reply(`you don't have any level/xp`).then(m => m.delete({
@@ -84,7 +84,5 @@ module.exports = {
                     });
             });
         }
-
-
-    }
+    // }
 };
