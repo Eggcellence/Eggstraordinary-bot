7cf9c7d16ddc37c6dd92c9d2922c9f9c6140ae71
A7U <abdseyada04@gmail.com>
2021-04-18 14:34:12 +0200

    Fix inventory and rank, fetch real user and not first user

--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -17,43 +17,42 @@ module.exports = {
         const discriminator = message.author.discriminator;
         const guildid = message.guild.id;
 
-        // if (args[0]) {
-        //     message.guild.members.fetch({
-        //         query: args[0]
-        //     }).then(e => {
-        //         if (!e.first()) return message.channel.send(`I've searched everywhere but I couldn't find someone with the name of \`${args[0]}\``);
-        //         let user = e.first().user;
+        if (args[0]) {
 
-        //         egg.query(`SELECT * FROM leveling WHERE guild = ${guildid} AND userid = ${user.id}`, (err, rows) => {
+            let user = message.guild.members.cache.find(u => u.user.username === args[0]).user;
 
-        //             if (rows.length === 0) return message.reply(`<:woah:833072727224614963> Oh my god. This user never chatted here to gain XP!`).then(m => m.delete({
-        //                 timeout: 5000
-        //             }));
+            egg.query(`SELECT * FROM leveling WHERE guild = ${guildid} AND userid = ${user.id}`, (err, rows) => {
 
-        //             let xp = rows[0].xp
-        //             let level = rows[0].level
-        //             let reqxp = 5 * (level ** 2) + 50 * level + 100
-        //             const rank = new canvacord.Rank()
-        //                 .setAvatar(user.avatarURL({format: 'png'}))
-        //                 .setCurrentXP(xp)
-        //                 .setRequiredXP(reqxp)
-        //                 .setLevel(level)
-        //                 .setRank(0, 0, display = false)
-        //                 .setLevelColor('#FFFFFF', '#f6b92b')
-        //                 .setStatus(user.presence.status)
-        //                 .setProgressBar("#f6b92b", "COLOR")
-        //                 .setUsername(user.username)
+                if (rows.length === 0) return message.channel.send(`<:woah:833072727224614963> Oh my god. This user never chatted here to gain XP!`).then(m => m.delete({
+                    timeout: 5000
+                }));
 
-        //                 .setDiscriminator(user.discriminator);
+                let xp = rows[0].xp
+                let level = rows[0].level
+                let reqxp = 5 * (level ** 2) + 50 * level + 100
+                const rank = new canvacord.Rank()
+                    .setAvatar(user.avatarURL({
+                        format: 'png',
+                        dynamic: true
+                    }))
+                    .setCurrentXP(xp)
+                    .setRequiredXP(reqxp)
+                    .setLevel(level)
+                    .setRank(0, 0, display = false)
+                    .setLevelColor('#FFFFFF', '#f6b92b')
+                    .setStatus(user.presence.status)
+                    .setProgressBar("#f6b92b", "COLOR")
+                    .setUsername(user.username)
 
-        //             rank.build()
-        //                 .then(data => {
-        //                     const attachment = new Discord.MessageAttachment(data, "RankCard.png");
-        //                     message.channel.send(attachment);
-        //                 });
-        //         });
-        //     })
-        // } else {
+                    .setDiscriminator(user.discriminator);
+
+                rank.build()
+                    .then(data => {
+                        const attachment = new Discord.MessageAttachment(data, "RankCard.png");
+                        message.channel.send(attachment);
+                    });
+            });
+        } else {
             egg.query(`SELECT * FROM leveling WHERE guild = ${guildid} AND userid = '${userid}'`, (err, rows) => {
 
                 if (rows.length === 0) return message.reply(`you don't have any level/xp`).then(m => m.delete({
@@ -84,5 +83,7 @@ module.exports = {
                     });
             });
         }
-    // }
+
+
+    }
 };
